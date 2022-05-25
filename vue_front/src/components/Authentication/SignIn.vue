907a3c468d55ebf6e1e72fa1a0f0243d1843a1d4
xingqiii <zuroant@protonmail.com>
2022-05-25 20:55:54 +0200

    auth

---
+++ vue_front/src/components/Authentication/SignIn.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="outer-login">
+    <div class="left"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.outer-login {
+  width: 500px;
+  height: 600px;
+  background-color: blue;
+}
+</style>
--- vue_front/src/views/Settings.vue
+++ vue_front/src/views/Settings.vue
@@ -1,42 +1,62 @@
 <template>
   <div class="settings-outer">
     <div class="settings-left">
-      <button>Auth</button>
-      <button>Others</button>
+      <!-- If Logged Out -->
+      <button>SignIn</button>
+      <button>SignUp</button>
+      <!-- If Logged In -->
+      <button>SignOut</button>
+      <button>Delete Account</button>
     </div>
     <TheSeparatorVue class="separator" />
-    <div class="settings-right">Hello</div>
+    <div class="settings-SignIn"><SignInVue /></div>
+    <!-- <div class="settings-SignUp">Hello</div>
+    <div class="settings-SignOut">Hello</div>
+    <div class="settings-Delete_Account">Hello</div> -->
   </div>
 </template>
 
 <script setup>
+import SignInVue from "@/components/Authentication/SignIn.vue";
 import TheSeparatorVue from "../components/TheSeparator.vue";
 </script>
 
 <style lang="scss" scoped>
 .settings-outer {
-  z-index: 100;
+  z-index: 12;
   position: absolute;
   left: 0;
   top: 0;
   height: 100%;
   width: 100%;
+  padding-left: 10%;
+  padding-top: 8%;
   display: flex;
-  justify-content: center;
+  justify-content: left;
 }
 
 .settings-left {
   display: flex;
   flex-direction: column;
+  padding-top: 10px;
+  button {
+    margin-top: 5px;
+  }
 }
 
 .separator {
-  --width: 2px;
-  --height: 500px;
-  --back-color: blue;
+  --width: 1px;
+  --height: calc(100vh - 20%);
+  --back-color: var(--heading-color);
+  transition: 1s ease-in-out background-color;
+  margin: 0px 10px;
 }
 
-.settings-right {
-  width: 500px;
+.settings-SignIn {
+  width: 92%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
